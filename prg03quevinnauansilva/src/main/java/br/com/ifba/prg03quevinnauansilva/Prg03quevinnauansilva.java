6f5a7f1426be60aa12c455b5e8b79147606c714d
email <quevinnauans@gmail.com>
2026-08-31 21:37:54 -0300

    estrutura inicial do projeto Maven

---
+++ prg03quevinnauansilva/src/main/java/br/com/ifba/prg03quevinnauansilva/Prg03quevinnauansilva.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.ifba.prg03quevinnauansilva;
+
+/**
+ *
+ * @author Quevin Nauan
+ */
+public class Prg03quevinnauansilva {
+
+    public static void main(String[] args) {
+        
+    }
+}
