3f96377973e352ca3bdc6752d8d961089e44d682
email <quevinnauans@gmail.com>
2026-09-16 09:28:05 -0300

    feat: encapsula classe de Usuario comgetters, setters e construtores

--- prg03quevinnauansilva/src/main/java/br/com/ifba/usuario/entity/Usuario.java
+++ prg03quevinnauansilva/src/main/java/br/com/ifba/usuario/entity/Usuario.java
@@ -7,13 +7,92 @@ package br.com.ifba.usuario.entity;
 public class Usuario {
     // Definindo atributos
     
-    public String nome;
-    public String cpf;
-    public String genero;
-    public String dataNascimento;
-    public String telefone;
-    public String email;
-    public String login;
-    public String senha;
+    private String nome;
+    private String cpf;
+    private String genero;
+    private String dataNascimento;
+    private String telefone;
+    private String email;
+    private String login;
+    private String senha;
+    
+    // Construtor vazio
+    public Usuario(){
+        
+    }
+    // Construtor com os atributos principais 
+        public Usuario(String nome, String cpf, String login, String senha){
+            this.nome = nome;
+            this.cpf = cpf;
+            this.login = login;
+            this.senha = senha;
+        }
+        
+    // getters e setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+        
     
 }
